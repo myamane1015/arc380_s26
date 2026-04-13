ee7dbc72378cbfcb01f1455bca63760119bc62e6
myamane1015 <my9689@princeton.edu>
2026-04-13 18:01:51 -0400

    .csv and configs

---
+++ workshops/final_project/config.py
@@ -0,0 +1,11 @@
+import yaml
+
+with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+workshop_path = config['workshop_path']
+max_height = config['max_height']
+link_name_sim = config['link_name_sim']
+link_name_real = config['link_name_real']  
+gripper_open = config['gripper_open']
+gripper_closed = config['gripper_closed']
